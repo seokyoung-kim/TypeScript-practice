d89effcc61966e0dbb82b1a806815aa354087abc
seokyoungkimm <seokyoungkimm@gmail.com>
2020-08-13 22:58:35 +0900

    typescript 기본 타입 test

--- src/practice.ts
+++ src/practice.ts
@@ -1,2 +1,18 @@
+let count = 0;
+count += 1;
+// count = '갑자기 분위기 문자열';
+
 const message: string = 'hello world';
-console.log(message);
+
+const done: boolean = true;
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ['hello', 'world'];
+
+// messages.push(1); // 숫자 입력 불가
+
+let mightBeUndefined: string | undefined = undefined; // string이거나 undefined이기 때문에 가능
+let nullableNumber: number | null = null;
+
+let color: 'red' | 'orange' | 'yellow' = 'red';
+color = 'yellow';
+// color = 'green'; // 에러 발생
