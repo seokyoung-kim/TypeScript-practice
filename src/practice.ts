8ebfde2ccf2cede175b4d597e0d073afddce9dbf
seokyoungkimm <seokyoungkimm@gmail.com>
2020-08-13 23:08:45 +0900

    함수에서 타입 정의

--- src/practice.ts
+++ src/practice.ts
@@ -1,18 +1,9 @@
-let count = 0;
-count += 1;
-// count = '갑자기 분위기 문자열';
+function sum(x:number, y:number): number {
+    return x + y;
+}
 
-const message: string = 'hello world';
+function sumArray(numbers: number[]) : number {
+    return numbers.reduce((acc, current) => acc + current, 0);
+}
 
-const done: boolean = true;
-const numbers: number[] = [1, 2, 3];
-const messages: string[] = ['hello', 'world'];
-
-// messages.push(1); // 숫자 입력 불가
-
-let mightBeUndefined: string | undefined = undefined; // string이거나 undefined이기 때문에 가능
-let nullableNumber: number | null = null;
-
-let color: 'red' | 'orange' | 'yellow' = 'red';
-color = 'yellow';
-// color = 'green'; // 에러 발생
+const total = sumArray([1,2,3,4,5]);
