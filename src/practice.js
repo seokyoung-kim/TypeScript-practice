d632ddeee6acdfcf8079d6932871950dd878c1b7
seokyoungkimm <seokyoungkimm@gmail.com>
2020-08-13 23:19:26 +0900

    interface 구현

--- src/practice.js
+++ src/practice.js
@@ -1,3 +1,8 @@
 "use strict";
-var message = 'hello world';
-console.log(message);
+function sum(x, y) {
+    return x + y;
+}
+function sumArray(numbers) {
+    return numbers.reduce(function (acc, current) { return acc + current; }, 0);
+}
+var total = sumArray([1, 2, 3, 4, 5]);
