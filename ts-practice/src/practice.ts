7b86637a5e8b710268c5c64eb26125ecd1c4c1e8
seokyoungkimm <seokyoungkimm@gmail.com>
2020-08-18 22:10:32 +0900

    type 함수 배열값 작성

--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,5 +1,7 @@
-function sum(x:number, y:number): number {
-    return x + y;
+function sumArray(numbers: number[]) : number {
+    return numbers.reduce((acc, current) => acc + current, 0)
 }
 
-const result = sum(1, 2);
+const total = sumArray([1,2,3,4,5]);
+
+console.log(total)
