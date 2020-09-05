2855830dc7cd363ebcc87e2b38f4475f38443d8f
seokyoungkimm <seokyoungkimm@gmail.com>
2020-09-05 21:08:13 +0900

    commit

--- ts-blockchain/index.ts
+++ ts-blockchain/index.ts
@@ -1,11 +1,7 @@
-const name = "nicolas",
-age = 24,
-gender = "male";
-
-const sayHi = (name, age, gender?) => {
+const sayHi = (name: string, age: number, gender?: string): void => {
     console.log(`hello ${name}, you are ${age}, you are a ${gender}`)
 };
 
-sayHi(name, age);
+sayHi("Kim", 31, "male");
 
 export {};
